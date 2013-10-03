4cac46eec0dc402227b9fbc733622f9aaa191641
Anton Romanovich <anthony.romanovich@gmail.com>
2013-10-03 19:12:33 +0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ MONGO_REPLICA_SET_NAME = 'grid_fs_set'
 IMAGE_FILTER_MODULE_RESIZE_LOCATION = '/internal/{mode}/{w}x{h}/{id}'
 IMAGE_FILTER_MODULE_ROTATE_LOCATION = '/internal/rotate/{angle}/{id}'
 
-AVERAGE_TASK_TIME = timedelta(seconds=60)  # Должно быть такое же, как и в настройках Unistore
+AVERAGE_TASK_TIME = timedelta(seconds=60 * 20)
 
 try:
     from settings_local import *
